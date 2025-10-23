69eee423d66360724167e4cbf4615905d04acc8c
vineethBotsa <103522660+vineethBotsa@users.noreply.github.com>
2025-10-23 18:18:52 +0530

    Update async_fifo_tb.v

--- async_fifo_tb.v
+++ async_fifo_tb.v
@@ -1,24 +1,67 @@
-`timescale 1ns/1ps
-module async_fifo_tb;
-  reg wclk = 0, rclk = 0, wrst_n, w_en, r_en;
-  reg [7:0] w_data;
-  wire [7:0] r_data;
-  wire full, empty;
+module async_fifo_TB;
 
-  // Clock generation
-  always #5  wclk = ~wclk;  // write clock
-  always #7  rclk = ~rclk;  // read clock
+  parameter DATA_WIDTH = 8;
 
-  // Instantiate FIFO
-  async_fifo_top #(8, 3) dut (
-    .wclk(wclk), .wrst_n(wrst_n), .w_en(w_en), .w_data(w_data),
-    .rclk(rclk), .rrst_n(wrst_n), .r_en(r_en),
-    .r_data(r_data), .full(full), .empty(empty)
-  );
+  wire [DATA_WIDTH-1:0] data_out;
+  wire full;
+  wire empty;
+  reg [DATA_WIDTH-1:0] data_in;
+  reg w_en, wclk, wrst_n;
+  reg r_en, rclk, rrst_n;
+
+  // Queue to push data_in
+  reg [DATA_WIDTH-1:0] wdata_q[$], wdata;
+
+  asynchronous_fifo as_fifo (wclk, wrst_n,rclk, rrst_n,w_en,r_en,data_in,data_out,full,empty);
+
+  always #10ns wclk = ~wclk;
+  always #35ns rclk = ~rclk;
+  
+  initial begin
+    wclk = 1'b0; wrst_n = 1'b0;
+    w_en = 1'b0;
+    data_in = 0;
+    
+    repeat(10) @(posedge wclk);
+    wrst_n = 1'b1;
+
+    repeat(2) begin
+      for (int i=0; i<30; i++) begin
+        @(posedge wclk iff !full);
+        w_en = (i%2 == 0)? 1'b1 : 1'b0;
+        if (w_en) begin
+          data_in = $urandom;
+          wdata_q.push_back(data_in);
+        end
+      end
+      #50;
+    end
+  end
 
   initial begin
-    wrst_n = 0; w_en = 0; r_en = 0; w_data = 0;
-    #20 wrst_n = 1;
-    // Add write/read sequences, monitors, $display, finish
+    rclk = 1'b0; rrst_n = 1'b0;
+    r_en = 1'b0;
+
+    repeat(20) @(posedge rclk);
+    rrst_n = 1'b1;
+
+    repeat(2) begin
+      for (int i=0; i<30; i++) begin
+        @(posedge rclk iff !empty);
+        r_en = (i%2 == 0)? 1'b1 : 1'b0;
+        if (r_en) begin
+          wdata = wdata_q.pop_front();
+          if(data_out !== wdata) $error("Time = %0t: Comparison Failed: expected wr_data = %h, rd_data = %h", $time, wdata, data_out);
+          else $display("Time = %0t: Comparison Passed: wr_data = %h and rd_data = %h",$time, wdata, data_out);
+        end
+      end
+      #50;
+    end
+
+    $finish;
+  end
+  
+  initial begin 
+    $dumpfile("dump.vcd"); $dumpvars;
   end
 endmodule
