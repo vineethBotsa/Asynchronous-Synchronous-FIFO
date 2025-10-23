9c3130f6a37c13f187c60b1bc9464c800b4971b8
vineethBotsa <103522660+vineethBotsa@users.noreply.github.com>
2025-10-23 18:17:35 +0530

    Update synchronizer.v

--- synchronizer.v
+++ synchronizer.v
@@ -1,14 +1,11 @@
-module synchronizer #(parameter WIDTH=3) (
-  input clk, rst_n,
-  input [WIDTH:0] d_in,
-  output reg [WIDTH:0] d_out
-);
+module synchronizer #(parameter WIDTH=3) (input clk, rst_n, [WIDTH:0] d_in, output reg [WIDTH:0] d_out);
   reg [WIDTH:0] q1;
   always@(posedge clk) begin
-    if (!rst_n) begin
+    if(!rst_n) begin
       q1 <= 0;
       d_out <= 0;
-    end else begin
+    end
+    else begin
       q1 <= d_in;
       d_out <= q1;
     end
